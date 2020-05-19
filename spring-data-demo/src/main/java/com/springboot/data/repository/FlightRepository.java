6cf86c1c8501d7c2bb28e185a391f16aa5aed4cb
nfebrian13 <febrian.nana@gmail.com>
2020-05-19 16:21:28 +0700

    commit test

--- spring-data-demo/src/main/java/com/springboot/data/repository/FlightRepository.java
+++ spring-data-demo/src/main/java/com/springboot/data/repository/FlightRepository.java
@@ -1,9 +1,22 @@
 package com.springboot.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.data.entity.Flight;
 
 public interface FlightRepository extends CrudRepository<Flight, Long> {
 
+	List<Flight> findByOrigin(String origin);
+
+	List<Flight> findByOriginAndDestination(String origin, String destination);
+
+    List<Flight> findByOriginIn(String ... origins);
+
+    List<Flight> findByOriginIgnoreCase(String origin);
+
+    Page<Flight> findByOrigin(String origin, Pageable pageRequest);
 }
